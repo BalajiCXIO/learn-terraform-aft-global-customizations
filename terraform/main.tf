c91b546b67189b1e815903aee3db1a839d508f3c
BalajiCXIO <balajip@cloudxsupport.com>
2022-08-04 17:40:52 +0530

    provider issue resolved

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,19 +1,3 @@
-terraform {
-    required_providers {
-        aws = {
-        source  = "hashicorp/aws"
-        version = "~> 3.27"
-        }
-    }
-
-    required_version = ">= 0.14.9"
-}
-
-provider "aws" {
-    profile = "default"
-    region  = "ap-south-1"
-}
-
 resource "aws_iam_role" "sample_role" {
     name = "testing_role_1"
 
